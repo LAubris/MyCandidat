b663452d4f8682ff1f76a955ee8605c7ca04aa4e
laubris <leo.aubrispro@gmail.com>
2021-03-16 10:30:44 +0100

    Bootstrap + page inscription + connexion Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6akPBVCWq7MYKCMW47MpS9IYAbdL7Xgo",
+  authDomain: "mycandidat-a2e10.firebaseapp.com",
+  projectId: "mycandidat-a2e10",
+  storageBucket: "mycandidat-a2e10.appspot.com",
+  messagingSenderId: "960342895050",
+  appId: "1:960342895050:web:ac15bbe2317c7210b70733"
+};
+
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'inscription',
+        loadChildren: () => import('../inscription/inscription.module').then(m => m.InscriptionPageModule)
       },
       {
         path: 'tab2',
