25c8d9e9d93c495c480d935fa9bf5b942a0c0194
CheruelQuentin <qquentincc@gmail.com>
2021-03-18 15:52:26 +0100

    image accueil

--- src/app/accueil/accueil.page.ts
+++ src/app/accueil/accueil.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireStorage } from '@angular/fire/storage';
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.page.html',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public afDB: AngularFireDatabase,
+    public afSG: AngularFireStorage
+  ) {
+    this.getImagesDatabase();
   }
 
+  ngOnInit() {
+  }  
+  images = [];
+  getImagesDatabase() {
+    //probleme ici
+    this.afDB.list('Images').snapshotChanges(['child_added']).subscribe(images => {
+      images.forEach(image => {
+        this.getImagesStorage(image);
+      });
+    });
+  }
+  getImagesStorage(image: any) {
+    const imgRef = image.payload.exportVal().ref;
+    this.afSG.ref(imgRef).getDownloadURL().subscribe(imgUrl => {
+      console.log(imgUrl);
+      this.images.push({
+        name: image.payload.exportVal().name,
+        url: imgUrl
+      });
+    });
+  }
 }
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
@@ -8,20 +9,8 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
-    path: 'inscription',
-    loadChildren: () => import('./inscription/inscription.module').then( m => m.InscriptionPageModule)
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
     path: 'accueil',
     loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
->>>>>>> 2cbd6c76e8fb0a586bb5e565945fb2fcb01da97a
   }
 
 ];
