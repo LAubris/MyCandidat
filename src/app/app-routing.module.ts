3dc64b6042d8bb58c9dad5003f0ffbbd87dae836
laubris <leo.aubrispro@gmail.com>
2021-03-18 16:08:50 +0100

    ajout etudiant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -10,18 +9,10 @@ const routes: Routes = [
   {
     path: 'inscription',
     loadChildren: () => import('./inscription/inscription.module').then( m => m.InscriptionPageModule)
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
+  },
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
     path: 'accueil',
     loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
->>>>>>> 2cbd6c76e8fb0a586bb5e565945fb2fcb01da97a
   }
 
 ];
--- src/app/inscription/inscription.page.ts
+++ src/app/inscription/inscription.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
+
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.page.html',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public afDB: AngularFireDatabase,
+    public firestore: AngularFirestore
+    )
+    {
+      this.candidats = firestore.collection('candidat').valueChanges();
 
+     }
   ngOnInit() {
   }
 
+  candidats: Observable<any[]>;
+  can_nom: string;
+  can_prenom: string;
+  can_mail: string;
+  can_tel: string;
+
+
+
+  addFirestore() {
+    this.firestore.collection('candidats').add({
+      can_nom: this.can_nom,
+      can_prenom: this.can_prenom,
+      can_mail: this.can_mail,
+      can_tel: this.can_tel,
+      });
+  }
 }
