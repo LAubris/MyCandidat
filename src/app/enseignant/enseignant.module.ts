8a5194ed98dfedf62b689a68b77be0a2a10d4ff7
CheruelQuentin <qquentincc@gmail.com>
2021-03-30 11:37:04 +0200

    odification de la page enseignant

--- src/app/enseignant/enseignant.module.ts
+++ src/app/enseignant/enseignant.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { IonicModule } from '@ionic/angular';
 
 import { EnseignantPageRoutingModule } from './enseignant-routing.module';
@@ -13,7 +13,8 @@ import { EnseignantPage } from './enseignant.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    EnseignantPageRoutingModule
+    EnseignantPageRoutingModule,
+    Ng2SearchPipeModule
   ],
   declarations: [EnseignantPage]
 })
--- src/app/enseignant/enseignant.page.ts
+++ src/app/enseignant/enseignant.page.ts
@@ -2,21 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @Component({
   selector: 'app-enseignant',
   templateUrl: './enseignant.page.html',
   styleUrls: ['./enseignant.page.scss'],
+  
 })
 export class EnseignantPage implements OnInit {
 
   constructor(public firestore: AngularFirestore, public afAuth: AngularFireAuth,) {
     
     this.candidats = firestore.collection('candidats').valueChanges();
+    this.inscriptions = firestore.collection('inscriptions').valueChanges();
+
+    
   }
 
-
-
-
+filterTerm: string;
 
   ngOnInit() {
   }
@@ -25,13 +28,23 @@ export class EnseignantPage implements OnInit {
   can_prenom: string;
   can_mail: string;
   can_tel: string;
+  candidat =[];
 
-
-
+inscriptions: Observable<any[]>;
+ins_date: String =new Date().getUTCDate() +"/"+(new Date().getUTCMonth()+1)+"/"+new Date().getFullYear();
 
   logout() {
     this.afAuth.signOut();
   }
+
+  test;
+recherche(){
+  this.candidat = [];
+  var that = this;
+  that.test = that.firestore.collection('candidats').valueChanges();
+  that.test.ref.orderBy('can_nom');
+}
+
 
 
 }
